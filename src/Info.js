29be3dad035fe61c66f450237a0c23546a7e83a1
Manuel Sosa <manuel_sosa_martinez@hotmail.com>
2021-01-10 21:42:03 -0300

    Layout display ready

--- src/Info.js
+++ src/Info.js
@@ -21,10 +21,9 @@ function Info() {
         <h4>Hello there! I'm </h4>
         <h1>Manuel Sosa</h1>
         <h3>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat"
+          A 21 years old non-binary software engineer and student, currently in
+          the 4th year of my career in Computer Science. Be it academically or
+          work-related, my goal is to keep on learning. Let's talk!
         </h3>
         {isMobile ? downloadCV : ""}
       </div>
--- src/Projects.js
+++ src/Projects.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Transition } from "react-transition-group";
 import "./css/Projects.css";
 import { useMediaQuery } from "react-responsive";
-import projectIcon from "./media/projects.png";
+
 import svIcon from "./media/svIcon.png";
 import graph from "./media/graph.png";
 import portfolioIcon from "./media/portfolio.png";
@@ -11,73 +11,75 @@ import svImage from "./media/svImage.png";
 import graphImage from "./media/graphImage.png";
 import moreImage from "./media/moreImg.png";
 
+import portfolioImage from "./media/portfolioImage.png";
+
 function Projects() {
   const [header, setHeader] = useState({
     tittle: "Select a Project",
-    description: <h3>"This is my list of projects"</h3>,
+    description: (
+      <h3>"This is the list of projects I'm currently working on!"</h3>
+    ),
     link: "",
     linkText: "",
-    icon: projectIcon,
+    img: "",
   });
   const secretVoldemort = {
     tittle: "Secret Voldemort",
     description: (
       <h3>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat"
+        This project is a web version of "Secret Voldemort", a popular board
+        game, soon to be deployed!. I'm working mainly on the frontend with
+        React and Redux. But it's not only the game! You can also create a user
+        and check your stats and match history.
       </h3>
     ),
     img: svImage,
     icon: svIcon,
-    link: "asdd",
-    linkText: "dasdas",
+    link: "https://github.com/csralvall/Baldomeros-Secret-Voldemort",
+    linkText: "Check the code!",
   };
   const graphColoring = {
     tittle: "Graph Coloring",
     description: (
       <h3>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat"
+        The famous graph coloring problem: color as many vertex with the less
+        amount of colors possible. This project was born long ago; we made a
+        program in C to do huge calculations very fast, and recently I'm working
+        with React to properly display the results!.
       </h3>
     ),
     img: graphImage,
     icon: graph,
-    link: "asdd",
-    linkText: "dasdas",
+    link: "https://github.com/ManuelSosaMartinez/Graph-Coloring",
+    linkText: "Check the code!",
   };
   const portfolio = {
     tittle: "This Portfolio",
     description: (
       <h3>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat"
+        My portfolio! I made it myself with React and I plan to keep shaping it
+        up over time. I'm also working on a friends portfolio, so soon you'll
+        probably see it up here!
       </h3>
     ),
-    img: "",
+    img: portfolioImage,
     icon: portfolioIcon,
-    link: "asdd",
-    linkText: "dasdas",
+    link: "https://github.com/ManuelSosaMartinez/portfolio",
+    linkText: "Check the code!",
   };
   const more = {
     tittle: "More on the way!",
     description: (
       <h3>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat"
+        I'm currently working on a fair lot of projects; just nothing to show
+        yet! As soon as I get more code written and a stable version, I'll be
+        updating this section and publishing the repos.
       </h3>
     ),
     img: moreImage,
     icon: plusIcon,
-    link: "asdd",
-    linkText: "dasdas",
+    link: "https://github.com/ManuelSosaMartinez",
+    linkText: "My GitHub!",
   };
   const ProjectList = [secretVoldemort, graphColoring, portfolio, more];
 
@@ -125,13 +127,19 @@ function Projects() {
               }}
             >
               <h1>{header.tittle}</h1>
-              <img
-                className={isMobile ? "Icon" : "Image"}
-                src={isMobile ? header.icon : header.img}
-              />
+              {header.img != "" ? (
+                <img
+                  className={isMobile ? "Icon" : "Image"}
+                  src={isMobile ? header.icon : header.img}
+                />
+              ) : (
+                ""
+              )}
               {header.description}
               {header.linkText !== "" ? (
-                <a href={header.link}>{header.linkText}</a>
+                <a target="_blank" href={header.link}>
+                  {header.linkText}
+                </a>
               ) : (
                 ""
               )}
